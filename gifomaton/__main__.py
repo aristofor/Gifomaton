961b03cc1d03066ec2291923eb2d61d884ed390d
Ari <ari@shelley.local>
2015-11-10 18:36:22 +0100

    Trouve le dossier de données

--- gifomaton/__main__.py
+++ gifomaton/__main__.py
@@ -1,2 +1,18 @@
 # coding: utf-8
 
+from os import environ
+from os.path import join, isdir
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Trouve le dossier de données
+try:
+    VAR_DIR = environ['GIFOMATON_DATA']
+except KeyError:
+    from os.path import dirname, realpath
+    VAR_DIR = join(dirname(dirname(realpath(__file__))),'var')
+
+if not isdir(VAR_DIR):
+    logging.critical("Data directory is missing : {}".format(VAR_DIR))
+    raise RuntimeError()
